e2211e538d5758041c3162be3c92cf2903bef29a
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-09-02 04:47:51 +0000

    .

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask,request,url_for, render_template, request, send_file
+from flask_pymongo import PyMongo
 from mongodb import connect_db, db_upload
 
 web_site = Flask(__name__)
 connect_db()
+# mongo=PyMongo(web_site)
+
+
 @web_site.route('/')
 #nothing required here ig
 def index():
 	return render_template('index.html')
 
 
-@web_site.route('/upload')
-# https://www.javatpoint.com/flask-file-uploading
-# https://www.youtube.com/watch?v=DsgAuceHha4&ab_channel=PrettyPrinted
+
+
+@web_site.route('/upload', methods=['POST'])
+								# https://www.javatpoint.com/flask-file-uploading
+								# https://www.youtube.com/watch?v=DsgAuceHha4&ab_channel=PrettyPrinted
 def upload_file():
-  #code for file upload
   passkey= ''
-  file_contents= ''#file loc
-  email= ''#user email
+  file_contents= ''		#file loc
+  email= ''						#user email
   to_email= ''
-  file_id = db_upload(passkey, file_contents, email, to_email)#returns generated file_id 
-  return render_template('upload.html', file_id= file_id)
+  # if 'email' in request.file:
+	# 		email=request.file['email']
+	# 		mongo.save_file(email.fileanme, email)
+
+	# if 'to_email' in request.file:
+	# 		to_email=request.file['to_email']
+	# 		mongo.save_file(to_email.fileanme, to_email)
+
+	# if 'passkey' in request.file:
+	# 		passkey=request.file['passkey']
+	# 		mongo.save_file(passkey.fileanme, passkey)
+  										#code for file upload
+  file_id = db_upload(passkey, file_contents, email, to_email)	#returns generated file_id 
+	return render_template('upload.html', file_id= file_id)
+
+
 
 
 @web_site.route('/download') 
